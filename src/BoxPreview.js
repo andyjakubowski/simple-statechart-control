c8a6af0da876cf48b21d28a77896d69ec5b2e51c
Andy Jakubowski <hello@andyjakubowski.com>
2021-02-17 18:56:26 +0100

    Successfully render all pseudo-class state combinations

--- src/BoxPreview.js
+++ src/BoxPreview.js
@@ -1,40 +1,38 @@
-import { useEffect, useCallback, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import './BoxPreview.scss';
 import PseudoStyler from './pseudostyler';
+import fastCartesian from 'fast-cartesian';
 
 const BoxPreview = function BoxPreview() {
-  const hoverRef = useRef(null);
-  const activeRef = useRef(null);
-  const focusRef = useRef(null);
+  let buttonRefs = useRef([]);
+  const pseudoClasses = [':hover', ':active', ':focus'];
+  const combinations = fastCartesian(pseudoClasses.map((_) => [true, false]));
+  const buttons = combinations.map((combArray, i) => (
+    <button ref={(el) => buttonRefs.current.push(el)} key={i}>
+      {`hover:\t${combArray[0]}\nactive:\t${combArray[1]}\nfocus:\t${combArray[2]}`}
+    </button>
+  ));
+
   useEffect(() => {
+    console.log('useEffect');
+    console.log('buttonRefs.current:', buttonRefs.current);
     (async function togglePseudoStyles() {
       const styler = new PseudoStyler();
       await styler.loadDocumentStyles();
 
-      styler.toggleStyle(hoverRef.current, ':hover');
-      styler.toggleStyle(activeRef.current, ':active');
-      styler.toggleStyle(focusRef.current, ':focus');
+      buttonRefs.current.forEach((buttonRef, buttonIndex) => {
+        const pseudoClassValues = combinations[buttonIndex];
+        pseudoClassValues.forEach((isClassActive, classIndex) => {
+          if (isClassActive) {
+            styler.toggleStyle(buttonRef, pseudoClasses[classIndex]);
+          }
+        });
+      });
     })();
+    return () => (buttonRefs.current = []);
   });
 
-  const handleClick = useCallback(() => {
-    console.log('click');
-  }, []);
-
-  return (
-    <div id="button-container">
-      <button onClick={handleClick}>box</button>
-      <button ref={hoverRef} onClick={handleClick}>
-        box
-      </button>
-      <button ref={activeRef} onClick={handleClick}>
-        box
-      </button>
-      <button ref={focusRef} onClick={handleClick}>
-        box
-      </button>
-    </div>
-  );
+  return <div id="button-container">{buttons}</div>;
 };
 
 export default BoxPreview;
