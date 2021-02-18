bb6b9c05862a9fab368304f2801735c674d8ba12
Andy Jakubowski <hello@andyjakubowski.com>
2021-02-18 10:21:08 +0100

    Render ORStateBoxes

--- src/ORStateBoxes.js
+++ src/ORStateBoxes.js
@@ -1,7 +1,56 @@
-import './ORStateBoxes.scss';
+import { createMachine } from 'xstate';
+import { useMachine } from '@xstate/react';
+
+const machineDefObj = {
+  initial: 'a',
+  states: {
+    a: {
+      on: {
+        click: {
+          target: 'b',
+        },
+      },
+    },
+    b: {
+      on: {
+        click: {
+          target: 'c',
+        },
+      },
+    },
+    c: {
+      on: {
+        click: {
+          target: 'a',
+        },
+      },
+    },
+  },
+};
+const stateKeys = Object.keys(machineDefObj.states);
+const machineDefObjects = stateKeys.map((key) => {
+  return {
+    ...machineDefObj,
+    initial: key,
+  };
+});
+const machines = machineDefObjects.map((machine) => createMachine(machine));
+
+const Box = function Box({ machine }) {
+  const [state] = useMachine(machine);
+  return <div className="box">State: {state.value}</div>;
+};
 
 const ORStateBoxes = function ORStateBoxes() {
-  return 'Hello ORStateBoxes!';
+  const boxEls = machines.map((machine, i) => (
+    <Box machine={machine} key={i} />
+  ));
+  return (
+    <div>
+      {boxEls}
+      {/* <button onClick={send}>click</button> */}
+    </div>
+  );
 };
 
 export default ORStateBoxes;
